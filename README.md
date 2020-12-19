9cdae86c6a057d896ae7b23c5783e19d6565e36f
Volkan Kumbasar <7557299+kumbasar@users.noreply.github.com>
2020-12-19 13:05:18 +0300

    email feature

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # CloudFoundry Application logs Alert
 
 A notification tool for cloud foundry applications 
+
+## Start Local email server
+
+```bash
+python -m smtpd -c DebuggingServer -n localhost:1025
+```
+
--- logalert.py
+++ logalert.py
@@ -3,7 +3,7 @@
 import argparse
 import json
 import smtplib
-
+from email.message import EmailMessage
 
 DEFAULT_CONFIG = 'config.json'
 
@@ -12,7 +12,27 @@ parser.add_argument("-c", "--config", help="Set config file. Example: config.jso
 
 args = parser.parse_args()
 
-config_json = ''
+
+class LogEmail():
+
+    subject = f'CF log alert'
+
+    def __init__(self, json_email):
+        self.smtp_url = json_email['smtp_url']
+        self.smtp_port = json_email['smtp_port']
+        self.sender_email = json_email['sender_email']
+        self.receiver_email = json_email['receiver_email']
+
+    def send_email(self, message):
+        msg = EmailMessage()
+        msg.set_content(message)
+        msg['Subject'] = self.subject
+        msg['From'] = self.sender_email
+        msg['To'] = self.receiver_email
+
+        with smtplib.SMTP(self.smtp_url, self.smtp_port) as m:
+            m.send_message(msg)
+
 
 try:
     with open(args.config, 'r') as json_file:
@@ -20,5 +40,6 @@ try:
 except (FileNotFoundError, IOError, json.decoder.JSONDecodeError):
     print("Wrong config file or path. ")
 
-print(config_json)
 
+le = LogEmail(config_json['email'])
+le.send_email('test')
